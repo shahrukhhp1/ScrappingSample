7e2d33d0d94452ae888adf764d7657f53e31192c
shahrukhhp1 <shahrukh_hp1@hotmail.com>
2021-04-22 22:49:10 +0500

    first commit

---
+++ WebScrapper_autorialad/WebScrapper_autorialad/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WebScrapper_autorialad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(343, 69);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(57, 20);
+            this.textBox5.TabIndex = 21;
+            this.textBox5.Text = "3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(343, 38);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 20);
+            this.textBox4.TabIndex = 20;
+            this.textBox4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "0/0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(54, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 134);
+            this.listBox1.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Message";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(343, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "D://outCSV";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(54, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "http://autorialadi.extra.bcv.org.ve/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Max Date (Empty box is Todays date)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Company Index";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(260, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Max delay time";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(266, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 262);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+
+
+    }
+}
+
---
+++ WebScrapper_autorialad/WebScrapper_autorialad/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebScrapper_autorialad
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driver = null;
+        // 9
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                driver = new ChromeDriver(@"C:\SeleniumWebDriver\chromedriver");
+            }
+            catch
+            {
+                MessageBox.Show("There was an error.Program will now close");
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        private void SomeMethod()
+        {
+            label2.Text = "Started";
+            label2.Refresh();
+            driver.Navigate().GoToUrl("http://autorialadi.extra.bcv.org.ve/operadorAladi/busqfecautorizacionaladi.jsp");
+
+            var company = extensionss.FindElement(driver, "//*[@id=\"inst\"]", 5);
+            if (company == null)
+            {
+                driver.Quit();
+                System.Windows.Forms.Application.Exit();
+            }
+
+            DateTime end = DateTime.Now;
+            //DateTime start = DateTime.Parse(textBox1.Text);
+            if (textBox2.Text == "")
+            {
+            }
+            else
+            {
+                end = DateTime.Parse(textBox2.Text);
+            }
+
+            var comp = new SelectElement(company);
+            var max = comp.Options.Count;
+            int stat = Convert.ToInt32(textBox4.Text);
+            for (int cps = stat; cps < max; cps++)
+            {
+                label3.Text = cps + 1 + "/" + max;
+                label3.Refresh();
+
+                comp.SelectByIndex(cps);
+                var nameofComp = comp.SelectedOption.Text;
+                label2.Text = "Working";
+                label2.Refresh();
+
+                var csv = new StringBuilder();
+
+
+                DateTime nEnd = end;
+                DateTime nStart = nEnd.AddMonths(-1).AddDays(1);
+
+                int line = 0;
+                int monthlyTry = 0;
+                while (monthlyTry < 6 || nEnd > end.AddYears(-4))
+                {
+                    var d1 = driver.FindElement(By.XPath("//*[@id=\"feini\"]"));
+                    d1.SendKeys(nStart.ToString("dd/MM/yyyy").Replace('-', '/'));
+
+                    var d2 = driver.FindElement(By.XPath("//*[@id=\"fefin\"]"));
+                    d2.SendKeys(nEnd.ToString("dd/MM/yyyy").Replace('-', '/'));
+
+                    var butt = driver.FindElement(By.XPath("//*[@id=\"enviar\"]"));
+                    butt.Click();
+                    line = GetTableContents(line, ref csv);
+                    if (File.Exists(textBox3.Text + "//" + GetSafeFilename(nameofComp) + ".csv") && csv.Length > 0)
+                    {
+                        File.AppendAllText(textBox3.Text + "//" + GetSafeFilename(nameofComp) + ".csv", csv.ToString());
+                        monthlyTry = 0;
+                    }
+                    else if (csv.Length > 0)
+                    {
+                        File.WriteAllText(textBox3.Text + "//" + GetSafeFilename(nameofComp) + ".csv", csv.ToString());
+                        monthlyTry = 0;
+                    }
+                    else if (csv.Length == 0)
+                        monthlyTry += 1;
+
+                    csv = new StringBuilder();
+                    while (line != 0)
+                    {
+                        line = NextPageExtraction(line, ref csv);
+                    }
+                    if (csv.Length > 0)
+                        File.AppendAllText(textBox3.Text + "//" + GetSafeFilename(nameofComp) + ".csv", csv.ToString());
+                    csv = new StringBuilder();
+
+                    driver.Navigate().GoToUrl("http://autorialadi.extra.bcv.org.ve/operadorAladi/busqfecautorizacionaladi.jsp");
+                    company = extensionss.FindElement(driver, "//*[@id=\"inst\"]", 5);
+                    comp = new SelectElement(company);
+                    comp.SelectByIndex(cps);
+
+                    nEnd = nEnd.AddMonths(-1);
+                    nStart = nEnd.AddMonths(-1).AddDays(1);
+                }
+
+                listBox1.Items.Add(nameofComp);
+
+                listBox1.Refresh();
+                driver.Navigate().GoToUrl("http://autorialadi.extra.bcv.org.ve/operadorAladi/busqfecautorizacionaladi.jsp");
+                GoWait();
+
+                company = driver.FindElement(By.XPath("//*[@id=\"inst\"]"));
+                comp = new SelectElement(company);
+            }
+            this.Focus();
+            label2.Text = "Done";
+            label2.Refresh();
+            MessageBox.Show("Done");
+        }
+
+        public string GetSafeFilename(string filename)
+        {
+
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+
+        }
+
+        private void GoWait()
+        {
+            Random r = new Random();
+            int stat = Convert.ToInt32(textBox5.Text);
+            int nRan = (r.Next(1, stat)) * 1000;
+            label2.Text = "Waiting " + nRan / 1000 + " sec";
+            label2.Refresh();
+            System.Threading.Thread.Sleep(nRan);
+            label2.Text = "Working";
+            label2.Refresh();
+        }
+
+        private int NextPageExtraction(int line, ref StringBuilder csv)
+        {
+            var page = driver.FindElement(By.XPath("//*[@id=\"pagineo\"]"));
+            var nn = page.GetAttribute("value");
+            var pagNo = nn.Substring(nn.LastIndexOf(" ") + 1);
+            if (Convert.ToInt32(pagNo) > line)
+            {
+                driver.FindElement(By.XPath("//*[@id=\"listDetalle\"]/div/img[2]")).Click();
+                GoWait();
+                if (line <= 50)
+                {
+                    try
+                    {
+                        driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    }
+                    catch
+                    {
+                        GoWait();
+                        driver.SwitchTo().Window(driver.WindowHandles[1]);
+                    }
+                }
+
+                line = GetTableContents(line, ref csv);
+                return line;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private int GetTableContents(int line, ref StringBuilder csv)
+        {
+            var table = extensionss.FindElement(driver, "/html/body/table[2]/tbody/tr[2]/td/div/table[1]/tbody", 1);
+            try
+            {
+                if (table == null || !table.Displayed)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (extensionss.FindElement(driver, "/html/body/table[1]/tbody/tr[3]/td[2]", 5) != null)
+                        {
+                            if (extensionss.FindElement(driver, "/html/body/table[1]/tbody/tr[3]/td[2]", 5).Text.Trim() == "Información no disponible")
+                            {
+                                table = null;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            table = extensionss.FindElement(driver, "/html/body/table[2]/tbody/tr[2]/td/div/table[1]/tbody", 5);
+                            if (table != null && table.Displayed)
+                                break;
+                        }
+                         
+                        GoWait();
+                    }
+                }
+            }
+            catch
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (extensionss.FindElement(driver, "/html/body/table[1]/tbody/tr[3]/td[2]", 5) != null)
+                    {
+                        if (extensionss.FindElement(driver, "/html/body/table[1]/tbody/tr[3]/td[2]", 5).Text.Trim() == "Información no disponible")
+                        {
+                            table = null;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        table = extensionss.FindElement(driver, "/html/body/table[2]/tbody/tr[2]/td/div/table[1]/tbody", 5);
+                        if (table != null && table.Displayed)
+                            break;
+                    }
+
+                    GoWait();
+                }
+            }
+            if (table != null)
+            {
+                IList<IWebElement> allRows = table.FindElements(By.TagName("tr"));
+                for (int i = 2; i < allRows.Count() - 1; i++)
+                {
+
+                    var row = allRows[i];
+                    IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+                    string cz = cells[0] != null ? cells[0].Text : "";
+                    string cf = cells[1] != null ? cells[1].Text : "";
+                    string cs = cells[2] != null ? cells[2].Text : "";
+                    //string ct = cells[3] != null ? cells[3].Text : "";
+                    //string cfo = cells[4] != null ? cells[4].Text : "";
+                    //string cfi = cells[5] != null ? cells[5].Text : "";
+                    //string csi = cells[6] != null ? cells[6].Text : "";
+                    //string cse = cells[7] != null ? cells[7].Text : "";
+                    // string ce = cells[8] != null ? cells[8].Text : "";
+                    string newLine = string.Format("{0},{1},{2},{3}", cz, cf, cs, Environment.NewLine);
+                    //string newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", cz, cf, cs, ct, cfo, cfi, csi, cse, Environment.NewLine);
+                    //foreach (var cell in cells)
+                    //{
+                    //    newLine += string.Format("{0},", cell.Text);
+                    //}
+                    //newLine += string.Format("{0}", Environment.NewLine);
+                    csv.Append(newLine);
+
+                    line += 1;
+                }
+            }
+            return line;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Quit();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SomeMethod();
+        }
+    }
+    public static class extensionss
+    {
+        public static IWebElement FindElement(this IWebDriver driver, string xname, int timeoutInSeconds)
+        {
+            try
+            {
+                if (timeoutInSeconds > 0)
+                {
+                    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                    return wait.Until(drv => drv.FindElement(By.XPath(xname)));
+                }
+                return driver.FindElement(By.XPath(xname));
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+    }
+}
